0db8e8fb7ab45e6fa6939faf69dd829d47ba55cd
Sharad Kharade <sharadkharade24@gmail.com>
2025-07-23 11:51:13 +0530

    "Local files/Load&decode JSON"

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:food_app/models/catalog.dart';
 import 'package:food_app/widgest/drawer.dart';
 import 'package:food_app/widgest/item_widget.dart';
-class HomePage extends StatelessWidget {
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days =30;
+
   final String name ="sharad kharade";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  loadData()async {
+    final catalogJson = await rootBundle.loadString("asset/files/catalog.json");
+    final decodedData = jsonDecode("catalog.json");
+    var productData = decodedData["products"];
+    print(productData);
+
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummylist = List.generate(50, (index) => CatalogModel.items[0]);
@@ -23,7 +47,6 @@ class HomePage extends StatelessWidget {
             },
           ),
         ),
-
       drawer: MyDrawer(),
     );
     }
